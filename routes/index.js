8577ef2ab31536dd96c30b568c5369d22291cff0
Henry Pan <legendkiller999@hotmail.com>
2021-10-01 16:57:42 -0400

    Update 1
    Implemented a home page

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET index page. */
+router.get('/', function(req, res, next) {
+  res.render('../pages/home', { title: 'Home'});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('../pages/home', { title: 'Home'});
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('../pages/about', { title: 'About Me'});
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('../pages/projects', { title: 'Projects'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('../pages/services', { title: 'Services'});
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('../pages/contact', { title: 'Contact'});
+});
+
+module.exports = router;
